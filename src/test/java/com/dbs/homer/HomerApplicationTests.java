59ee77eaf9f33278957437972c955fbd9aa09704
강지원 <kangji149@naver.com>
2023-12-07 15:57:27 +0900

    initial commit

---
+++ src/test/java/com/dbs/homer/HomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbs.homer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
